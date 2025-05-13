f6bf0c837c73930bfe863724a4b029811e246a41
VladislavAduenko <adyenko.job@gmail.com>
2025-05-14 00:52:52 +0300

    isAnagram solution

--- src/main.ts
+++ src/main.ts
@@ -41,3 +41,35 @@ update(person1, { age: 25, name: "Katya" });
 console.log("After 2st update:", person1);
 
 // update(person1, { age: "25", namee: "Ivan", education: "Doctor" }); - ERROR
+
+
+
+// Write the function isAnagram taking two strings and returning true 
+// if the second string is an anagram of the first one
+// Anagram of an string is another string with the same letters and their occurrences as in the first one
+
+function isAnagram(str1: string, str2: string): boolean {
+    let result = true;
+
+    if (str1.length !== str2.length) {
+        result = false;
+    } 
+    const counter: { [key: string]: number } = {};
+    for(const char of str1.toLowerCase()) {
+        counter[char] = (counter[char] || 0) + 1;
+    }
+    for(const char of str2.toLowerCase()) {
+        if (!counter[char]) {
+            result = false;
+        } 
+        counter[char]--;
+        if (counter[char] === 0) {
+            delete counter[char];
+        }
+    }
+    return result
+}
+console.log(isAnagram("Кот", "кот")); 
+console.log(isAnagram("кот", "ток")); 
+console.log(isAnagram("Кот", "ток")); 
+console.log(isAnagram("кот", "кот")); 
